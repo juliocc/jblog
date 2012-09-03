22b4b808bc3a54b4391f4b5346b0213826dad258
Julio Castillo <juliocc@gmail.com>
2012-09-03 04:05:07 -0430

    About page

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 from django.core.urlresolvers import reverse
 from django.utils.functional import lazy
 
@@ -8,5 +8,7 @@ reverse_lazy = lazy(reverse, str)
 
 urlpatterns = patterns('',
     url(r'$^', RedirectView.as_view(url=reverse_lazy('blog_index')), name='home'),
+    url(r'^pages/about/$', 
+        TemplateView.as_view(template_name='about.html'), name='page_about'),
     (r'', include('apps.blog.urls')),
 )
