05adf6d306eb17bcc42ec677863fd777b449ba41
Julio Castillo <juliocc@gmail.com>
2012-09-02 22:50:17 -0430

    TinyMCE for content field

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -1,13 +1,15 @@
 from django.template.defaultfilters import slugify
 from django import forms
+from django import forms
 
 from google.appengine.ext.db import djangoforms
 
 from bootstrap.forms import BootstrapMixin, Fieldset
 from apps.blog.models import Entry
+from apps.blog.widgets import TinyMCEEditor
 
 class EntryForm(BootstrapMixin, djangoforms.ModelForm):
-    content = forms.Textarea
+    content = forms.CharField(widget=TinyMCEEditor())
 
     def clean(self):
         self.cleaned_data['slug'] = str(slugify(self.cleaned_data['title']))
