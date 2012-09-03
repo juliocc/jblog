8faaf8e93b2046864013d7e388f17ba0deea948a
Julio Castillo <juliocc@gmail.com>
2012-09-03 00:03:09 -0430

    allow spaces and dashes on tags

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # Diagnosis urls...
     url(r'^$', BlogIndex.as_view(), name='blog_index'),
     url(r'^all/$', AllIndex.as_view(), name='all_index'),
-    url(r'^tag/(?P<tag>\w+)/$', TagView.as_view(), name='tag_index'),
+                       url(r'^tag/(?P<tag>[\w _-]+)/$', TagView.as_view(), name='tag_index'),
     url(r'^entries/(?P<slug>[\w\d_-]+)/$', EntryView.as_view(), name='entry_view'),
     url(r'^post/$', entry_create, name='entry_create'),
     url(r'^edit/(?P<slug>[\w\d_-]+)/$', entry_create, name='entry_edit'),
