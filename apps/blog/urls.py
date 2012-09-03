a7c3c281e2cd2c3b94eaacb7bc369b88fff34115
Julio Castillo <juliocc@gmail.com>
2012-09-02 19:47:05 -0430

    Entry view, tag index and some refactoring.

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, url
 
-from apps.blog.views import BlogIndex
+from apps.blog.views import BlogIndex, TagView, EntryView
 
 urlpatterns = patterns('',
     # Diagnosis urls...
     url(r'^$', BlogIndex.as_view(), name='blog_index'),
+    url(r'^tag/(?P<tag>\w+)/$', TagView.as_view(), name='tag_index'),
+    url(r'^entries/(?P<slug>\w+)/$', EntryView.as_view(), name='entry_view'),
 )
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -4,22 +4,47 @@ from django.views.generic import TemplateView
 from apps.blog.models import Entry
 from helpers.paginator import GAEPaginator
 
-class BlogIndex(TemplateView):
-  http_method_names = ['get']
-  template_name = 'blog/index.html'
+class EntriesView(TemplateView):
+    http_method_names = ['get']
+    template_name = 'blog/index.html'
 
-  def get_context_data(self, **kwargs):
-    context = super(BlogIndex, self).get_context_data(**kwargs)
-    query = Entry.all().filter('status =', 'published') \
-                 .order('-published_at')
-    paginator = GAEPaginator(query, 10)
-  
-    try:
-      entries = paginator.page(self.request.GET.get('page', '1'))
-    except PageNotAnInteger:
-      entries = paginator.page(1)
-    except EmptyPage:
-      entries = paginator.page(paginator.num_pages)
+    def get_page(self, query):
+        paginator = GAEPaginator(query, 10)
+        try:
+            entries = paginator.page(self.request.GET.get('page', '1'))
+        except PageNotAnInteger:
+            entries = paginator.page(1)
+        except EmptyPage:
+            entries = paginator.page(paginator.num_pages)
 
-    context['entries'] = entries
-    return context
+        return entries
+
+    def get_context_data(self, **kwargs):
+        context = super(EntriesView, self).get_context_data(**kwargs)
+        entries = self.get_page(self.get_query())
+        context['entries'] = entries
+        return context
+      
+
+class BlogIndex(EntriesView):
+    def get_query(self):
+        return Entry.all().filter('status =', 'published') \
+                          .order('-published_at')
+
+
+class TagView(EntriesView):
+    def get_query(self):
+        return Entry.all().filter('status =', 'published') \
+                          .filter('tags =', self.kwargs['tag']) \
+                          .order('-published_at')
+
+class EntryView(TemplateView):
+    template_name = 'blog/view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EntryView, self).get_context_data(**kwargs)
+        slug = self.kwargs['slug']
+        entry = Entry.all().filter('slug =', slug).get()
+        # TODO: handle no results
+        context['entry'] = entry
+        return context
